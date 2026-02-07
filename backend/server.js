90246d25a25b93c0589fbf6b09d98821aadfb8f9
scriptWithKishan <kishan.2785.2003@gmail.com>
2026-02-07 04:31:44 +0000

    Initializing project with: MongoDB, Tailwind

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import mongoose from "mongoose"
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+dotenv.config()
+
+// Connect to MongoDB
+const { MONGO_URI } = process.env
+
+  ; (async function () {
+    try {
+      await mongoose.connect(MONGO_URI)
+      console.log('MongoDB server connected successfully')
+    } catch (err) {
+      console.error(`Error in connecting to MongoDB ${err.message}`)
+      process.exit(1)
+    }
+  })()
+
+// Running the server 
+const { PORT } = process.env
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-4xl font-medium'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
